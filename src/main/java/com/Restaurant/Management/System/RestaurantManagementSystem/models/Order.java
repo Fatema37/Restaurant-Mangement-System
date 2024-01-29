4584fc9a6f0c1634df61403ecc0e5c99bc25ae79
Fatema Rangoonwala <frangoonwala@adobe.com>
2024-01-29 20:33:12 +0530

    order modal

--- src/main/java/com/Restaurant/Management/System/RestaurantManagementSystem/models/Order.java
+++ src/main/java/com/Restaurant/Management/System/RestaurantManagementSystem/models/Order.java
@@ -8,6 +8,6 @@ import java.util.Map;
 @Getter
 @Setter
 public class Order extends BaseModel{
-    private Customer customer;
+    private CustomerSession customerSession;
     private Map<MenuItem, Integer> orderedItems;
 }
